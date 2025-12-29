2d280e99f3e3ae702bded7969079c922e4a1f690
bobbyrichard250-tech <bobbyrichard250@gmail.com>
2025-12-29 13:54:40 +0100

    ff

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function convert(){
     if(radio1.checked){
         temp = Number(numberbox.value)
         temp = temp * (9/5) + 32
-        document.getElementById("display").textContent =  temp.toFixed(1)+"*C"
+        document.getElementById("display").textContent =  temp.toFixed(1)+"*F"
     }
     else if(radio2.checked){
         temp = Number(numberbox.value)
