ab615e77e7eafee841cbdbfa839282da3d504444
bobbyrichard250-tech <bobbyrichard250@gmail.com>
2026-01-02 21:50:13 +0100

    jj

--- currency2.js
+++ currency2.js
@@ -5,17 +5,17 @@ function convert() {
 
     switch(hello) {
         case "USD":
-            result = input / 1455.21;
+            result = input * 1455.21;
             break;
         case "EUR":
-            result = input / 1.08;
+            result = input * 1.08;
             break;
         case "GBP":
-            result = input / 0.75;
+            result = input * 0.75;
             break;
         default:
             result = 0;
     }
 
-    document.getElementById("display").innerHTML = result.toFixed(2);
+    document.getElementById("display").innerHTML = result.toFixed(2)+"naira";
 }
