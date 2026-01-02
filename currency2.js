3b0e252de7c9960b64e0187f048423b4cb0b6650
bobbyrichard250-tech <bobbyrichard250@gmail.com>
2026-01-02 21:30:53 +0100

    hh

--- currency2.js
+++ currency2.js
@@ -5,13 +5,13 @@ function convert() {
 
     switch(hello) {
         case "USD":
-            result = input * 1455.21;
+            result = input / 1455.21;
             break;
         case "EUR":
-            result = input * 1.08;
+            result = input / 1.08;
             break;
         case "GBP":
-            result = input * 0.75;
+            result = input / 0.75;
             break;
         default:
             result = 0;
